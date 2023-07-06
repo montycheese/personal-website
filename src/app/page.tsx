6d2e86856d4a529772fd59181f35eb62667fb166
Montana Wong <montanawong@gmail.com>
2023-07-05 19:16:29 -0700

    add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,22 @@ import React from "react";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import Script from 'next/script';
+
 
 export default function Home() {
   return (
       <div className="app">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-GXBJ5T0SDT" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-GXBJ5T0SDT');
+        `}
+      </Script>
         <MainContent />
         <Footer />
       </div>
